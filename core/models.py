6652583d5469a60ce957e6e3e7fe6c82a2e74010
barbearialbipi <djangobarbearia@gmail.com>
2025-12-04 03:11:54 +0000

    Melhoria Funcional Final

--- core/models.py
+++ core/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from datetime import date
 
+# --- LISTAS DE OPÇÕES ATUALIZADA ---
 OPCOES_SERVICOS = [
-    ('DEGRADE', 'Degradê'), ('SOCIAL', 'Social'), ('PEZIM', 'Pezim'),
-    ('BARBA', 'Barba'), ('SOBRANCELHA', 'Sobrancelha'), ('LUZES', 'Luzes'),
-    ('PLATINADO', 'Platinado'), ('ALISAMENTO', 'Alisamento'),
-    ('PIGMENTACAO', 'Pigmentação'), ('COMPLETO', 'Combo Completo'), ('OUTRO', 'Outro')
+    ('TRADICIONAL', 'Tradicional'),
+    ('PEZIM', 'Pezim'),
+    ('DEGRADE', 'Degradê'),
+    ('NAVALHADO', 'Navalhado'),
+    ('SOCIAL', 'Social'),
+    ('BARBA', 'Barba'),
+    ('CONSULTORIA', 'Consultoria de Visagismo'),
+    ('ABORDAGEM', 'Abordagem de Visagismo'),
 ]
 
 OPCOES_BARBEIROS = [('LUCAS', 'Lucas Borges'), ('ALUIZIO', 'Aluízio'), ('ERIK', 'Erik')]
 OPCOES_VENDEDORES = OPCOES_BARBEIROS + [('FABRICIO', 'Fabrício')]
 OPCOES_PAGAMENTO = [('DINHEIRO', 'Dinheiro'), ('PIX', 'Pix'), ('CARTAO', 'Cartão'), ('MISTO', 'Misto')]
+
+# --- TABELAS ---
 
 class Agendamento(models.Model):
     data = models.DateField(default=date.today)
@@ -19,12 +26,14 @@ class Agendamento(models.Model):
     barbeiro = models.CharField(max_length=20, choices=OPCOES_BARBEIROS)
     servico = models.CharField(max_length=20, choices=OPCOES_SERVICOS)
     com_barba = models.BooleanField(default=False)
+    
     forma_pagamento = models.CharField(max_length=20, choices=OPCOES_PAGAMENTO)
     valor_total = models.DecimalField(max_digits=8, decimal_places=2)
-    
-    # Misto
+
+    # Campos para Pagamento Misto
     valor_1 = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     tipo_pagamento_1 = models.CharField(max_length=20, blank=True, null=True)
+    
     valor_2 = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     tipo_pagamento_2 = models.CharField(max_length=20, blank=True, null=True)
 
@@ -40,4 +49,3 @@ class Saida(models.Model):
     data = models.DateField(default=date.today)
     descricao = models.CharField(max_length=200, verbose_name="Descrição da Saída")
     valor = models.DecimalField(max_digits=8, decimal_places=2)
-    # Categoria removida conforme solicitado
