ff4aeeed9ac76946845d7636eaef4e0a65ebed9a
barbearialbipi <djangobarbearia@gmail.com>
2025-12-05 11:08:04 -0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from django.urls import path
+# IMPORTANTE: Adicionei esta linha abaixo para o TemplateView funcionar
+from django.views.generic import TemplateView 
 from core.views import home, login_view, deletar_item
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', login_view, name='login'),
-    path('', home, name='home'),
     
-    # Rota mágica para deletar qualquer coisa do Sheets
-    # Ex: /deletar/agendamento/54/ (Apaga linha 54 da aba Agendamentos)
+    # Login e Home
+    path('login/', login_view, name='login'),
+    path('', home, name='home'), # A home é a raiz
+    
+    # Rota para deletar itens
     path('deletar/<str:tipo>/<int:row_id>/', deletar_item, name='deletar_item'),
+    
+    # --- ROTAS PWA (Service Worker e Manifest) ---
+    # Nota: Certifique-se de que manifest.json e sw.js estão na pasta 'templates'
     path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json')),
     path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/javascript')),
 ]
